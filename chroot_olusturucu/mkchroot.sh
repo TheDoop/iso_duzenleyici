0aa2ef1ad67d00dd5a2d544db5c624e263ced2b7
parduscix <aliriza.keskin@pardustopluluk.org>
2018-01-15 22:51:36 +0000

    update v3

---
+++ chroot_olusturucu/mkchroot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ..
+debootstrap  --no-check-gpg  --arch=$(cat ./config/arch)  $(cat ./config/dist)  ./chroot  $(cat ./config/pool)
+chroot chroot apt-get update
+chroot chroot apt-get install live-config live-boot curl
+
