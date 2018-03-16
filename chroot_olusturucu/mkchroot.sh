e7fdd820983d2014b6f9e0792d673b02b5050968
İbrahim YÖNDEMLİ <doopthe@gmail.com>
2018-03-16 22:51:59 +0300

    Update mkchroot.sh

--- chroot_olusturucu/mkchroot.sh
+++ chroot_olusturucu/mkchroot.sh
@@ -2,5 +2,5 @@
 cd ..
 debootstrap  --no-check-gpg  --arch=$(cat ./config/arch)  $(cat ./config/dist)  ./chroot  $(cat ./config/pool)
 chroot chroot apt-get update
-chroot chroot apt-get install live-config live-boot curl
+chroot chroot apt-get install live-config live-boot curl grub2
 
